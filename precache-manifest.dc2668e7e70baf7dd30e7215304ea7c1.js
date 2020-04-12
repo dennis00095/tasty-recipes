eb5c5e8b38fbbdbde3e5f6e77512d4dff48a6f98
dennis00095 <mwongeladennis9@gmail.com>
2020-04-12 20:28:16 +0300

    Updates

---
+++ precache-manifest.dc2668e7e70baf7dd30e7215304ea7c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4e9407622e2872fffafe5909f2626d",
+    "url": "/tasty-recipes/index.html"
+  },
+  {
+    "revision": "1a5a713d8fa806c393f3",
+    "url": "/tasty-recipes/static/css/main.6208e0c0.chunk.css"
+  },
+  {
+    "revision": "e430e9db00d9ddbc5689",
+    "url": "/tasty-recipes/static/js/2.5406a3c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tasty-recipes/static/js/2.5406a3c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a5a713d8fa806c393f3",
+    "url": "/tasty-recipes/static/js/main.46858d98.chunk.js"
+  },
+  {
+    "revision": "d9938078d0e795a4e7ac",
+    "url": "/tasty-recipes/static/js/runtime-main.5863b3d6.js"
+  }
+]);
